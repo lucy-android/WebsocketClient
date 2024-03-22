3eb54003af2e1eb437464f84650427fed32b9bb0
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-03-22 23:14:30 +0300

    add code to check that the loopback address is reachable

--- app/src/main/java/com/example/android/architecture/blueprints/websocketclient/ChatboxActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/websocketclient/ChatboxActivity.kt
@@ -5,7 +5,13 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.github.nkzawa.socketio.client.IO
 import com.github.nkzawa.socketio.client.Socket
-import java.net.URISyntaxException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.net.InetAddress
+
 
 class ChatboxActivity : AppCompatActivity() {
 
@@ -15,15 +21,41 @@ class ChatboxActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatbox)
         Nickname = intent.extras!!.getString(MainActivity.NICKNAME)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            val host = InetAddress.getLoopbackAddress()
+            Log.d("APP_TAG", "is the address reachable: host.isReachable(1000) = ${host.isReachable(1000)}")
+        }
+
         try {
             socket = IO.socket("http://10.0.2.2:3000")
 
             socket?.connect()
             Log.d("APP_TAG", "is the socket connected: ${socket?.connected()}")
-            socket?.emit("join", Nickname)
+            socket?.emit("join ", Nickname)
         } catch (e: Exception) {
             Log.d("APP_TAG", "onCreate: ${e.cause}")
             e.printStackTrace()
+        }
+    }
+
+
+    fun isAddressReachable(address: String): Boolean {
+        return try {
+            // Start the process to ping the address
+            val process = Runtime.getRuntime().exec("/system/bin/ping -c 1 $address")
+
+            // Wait for the process to finish
+            val exitCode = process.waitFor()
+
+            // Check if the ping was successful (exit code 0 means success)
+            exitCode == 0
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+            false
         }
     }
 }
