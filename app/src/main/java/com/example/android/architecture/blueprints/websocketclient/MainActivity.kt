5b6325a9aebab86e4800ccdc7b51f8da3d827da6
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2024-03-28 23:42:38 +0300

    remove comments

--- app/src/main/java/com/example/android/architecture/blueprints/websocketclient/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/websocketclient/MainActivity.kt
@@ -16,18 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //call UI components  by id
         btn = findViewById<View>(R.id.enterchat) as Button
         nickname = findViewById<View>(R.id.nickname) as EditText
         btn!!.setOnClickListener {
-            //if the nickname is not empty go to chatbox activity and add the nickname to the intent extra
             if (nickname!!.text.toString().isNotEmpty()) {
                 val i = Intent(
                     this@MainActivity,
                     ChatboxActivity::class.java
                 )
 
-                //retreive nickname from EditText and add it to intent extra
                 i.putExtra(NICKNAME, nickname!!.text.toString())
                 startActivity(i)
             }
